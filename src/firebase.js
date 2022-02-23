dd8ca05fdcacc80b1e20b4ae55ffc88bc0916054
whk319 <herb.hhr@gmail.com>
2022-02-24 09:01:10 +1000

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,23 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5Kj7NU256TKVncdOCy_TELSmdRrEAH4c",
-  authDomain: "task-manager-1e712.firebaseapp.com",
-  projectId: "task-manager-1e712",
-  storageBucket: "task-manager-1e712.appspot.com",
-  messagingSenderId: "935257286219",
-  appId: "1:935257286219:web:d88cfe3837e8e9a4e2cfce"
+  apiKey: "AIzaSyDQCbF3gm4fBxypu-Makzqvh6l_XDNvTKg",
+  authDomain: "bonus-hunter-9844b.firebaseapp.com",
+  databaseURL: "https://bonus-hunter-9844b-default-rtdb.firebaseio.com",
+  projectId: "bonus-hunter-9844b",
+  storageBucket: "bonus-hunter-9844b.appspot.com",
+  messagingSenderId: "230297996032",
+  appId: "1:230297996032:web:eca4b79613e41d1f3b01e5",
+  measurementId: "G-GNJZX41R91"
 };
 
 // Initialize Firebase and Firestore
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+const analytics = getAnalytics(app);
 
 export {db}
